934600b2722970bb6ae85d7d985d738f5d8108a4
Michael Oldacre <mloldacre@live.com>
2021-04-29 21:10:33 -0400

    Initial Setup

---
+++ JavaScript/webInputs.js
@@ -0,0 +1,10 @@
+//! Prompt for browser
+const yourName = prompt('Please enter your name:');
+//! Pop up display box that shows what you want
+alert(`Your name is: ${yourName}`);
+
+
+
+let consent;
+//! Confirmation box pop up
+consent = confirm('Yes or no?');
