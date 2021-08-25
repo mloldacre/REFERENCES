a7c7b1341752be7f9038a4a6b57a52ed91b8f5bb
Michael Oldacre <65476228+mloldacre@users.noreply.github.com>
2021-08-25 19:49:29 -0400

    Co-authored-by: Alexis <Nvlt@users.noreply.github.com>
    Platform physics created

--- html_css/JS/moveTheBox.js
+++ html_css/JS/moveTheBox.js
@@ -5,24 +5,40 @@ let width = myCanvas.width = window.innerWidth;
 let height = myCanvas.height = window.innerHeight;
 
 let sqrX = 450, sqrY = 200
-let crlX = 450, crlY = 200
+let pltX = 600, pltY = 600
+let pltWIDTH = 200, pltHEIGHT = 10
+let sqrWIDTH = 50, sqrHEIGHT = 50
+let crlX = 10, crlY = 10
 let SPEED = 1
 let VELOCITY_X = 0, VELOCITY_Y = 0;
+let GRAVITY = 1;
+let PLTCOLLISION = false;
+let GRNDCOLLISION = false;
+//Direction Vector
+let DIRECTION = {
+  x: 0,
+  y: 0
+}
 
 document.addEventListener('keydown', (evt) => {
 
-  if (evt.key === "ArrowUp") {
-    VELOCITY_Y -= SPEED
-  }
-  if (evt.key === "ArrowDown") {
-    VELOCITY_Y += SPEED
-  }
+  // if (evt.key === "ArrowUp") {
+  //   DIRECTION.y = -1;
+  // }
+  // if (evt.key === "ArrowDown") {
+  //   DIRECTION.y = 1;
+  // }
   if (evt.key === "ArrowRight") {
-    VELOCITY_X += SPEED
+    DIRECTION.x = 1;
   }
   if (evt.key === "ArrowLeft") {
-    VELOCITY_X -= SPEED
+    DIRECTION.x = -1;
   }
+  if(evt.key == " ")
+  {
+    VELOCITY_Y -= 30;
+  }
+  
   
   switch (evt.key){
     case "w":
@@ -42,6 +58,40 @@ document.addEventListener('keydown', (evt) => {
 
   console.log('Key Down :>> ', evt);
 })
+document.addEventListener('keyup', (evt) => {
+
+  // if (evt.key === "ArrowUp") {
+  //   DIRECTION.y = 0;
+  // }
+  // if (evt.key === "ArrowDown") {
+  //   DIRECTION.y = 0;
+  // }
+  if (evt.key === "ArrowRight") {
+    DIRECTION.x = 0;
+  }
+  if (evt.key === "ArrowLeft") {
+    DIRECTION.x = 0;
+  }
+  
+  
+  switch (evt.key){
+    case "w":
+    crlY -= SPEED
+    break;
+    case "s":
+    crlY += SPEED
+    break;
+    case "d":
+    crlX += SPEED
+    break;
+    case "a":
+    crlX -= SPEED
+    break;
+  }
+  
+
+  console.log('Key UP :>> ', evt);
+})
 
 function draw() {
 
@@ -53,17 +103,25 @@ function draw() {
   // Square
   context.beginPath()
   context.fillStyle = "#000"
-  context.fillRect(sqrX, sqrY, 50, 50)
+  context.fillRect(sqrX, sqrY, sqrWIDTH, sqrHEIGHT)
+  
+  // Platform
+  context.beginPath()
+  context.fillStyle = "#EE5846"
+  context.fillRect(pltX, pltY, pltWIDTH, pltHEIGHT)
 
-  // Circle
+  // Circle 'sun'
   context.beginPath()
   context.arc(crlX, crlY, 70, 0, 2 * Math.PI);
   context.fillStyle = 'rgba(55,255,21,0.4)';
   context.fill()
 
-
-  sqrX += VELOCITY_X;
-  sqrY += VELOCITY_Y;
+  //Add Velocity Based on direction
+  VELOCITY_X += (DIRECTION.x * SPEED)
+  VELOCITY_Y += (GRAVITY * SPEED)
+  //Apply Velocity to box;
+  sqrX += (VELOCITY_X);
+  sqrY += (VELOCITY_Y);
   
   // *Automatic Decelerating
  VELOCITY_X *= .97
@@ -71,7 +129,60 @@ function draw() {
 
   // crlX += .3;
   // crlY += .1;
-
+  
+  // * Boundaries
+  if (sqrX > width){
+    sqrX = 1
+  } else if (sqrX <= -50){
+    sqrX = (width - 1)
+  }
+  
+  //! This one
+  if (sqrY > height - 50){
+    //! this stuff
+    sqrY = height - 50;
+    GRNDCOLLISION = true;
+    GRAVITY = 0;
+    VELOCITY_Y = 0;
+  }
+  else if(!PLTCOLLISION)
+  {
+    GRAVITY = 1;
+  }
+  else
+  {
+    GRNDCOLLISION = false;
+  }
+  //else if (sqrY <= 0){
+  //   sqrY = (height - 1)
+  // }
+  
+  // Platform Physics 🤷🏾‍♂️
+  if ((sqrX > (pltX - (sqrWIDTH - 5 ))) && (sqrX < (pltWIDTH + pltX))){
+    if(sqrY + 50 >= pltY && sqrY < pltY+pltHEIGHT)
+    {
+      PLTCOLLISION = true;
+      sqrY = pltY - 50;
+      GRAVITY = 0;
+      VELOCITY_Y = 0;
+    }
+    else if(!GRNDCOLLISION)
+    {
+      GRAVITY = 1;
+    }
+    else
+    {
+      PLTCOLLISION = false;
+    }
+  }
+  else if(!GRNDCOLLISION)
+  {
+    GRAVITY = 1;
+  }
+  else
+  {
+    PLTCOLLISION = false;
+  }
   requestAnimationFrame(draw)
 }
 
